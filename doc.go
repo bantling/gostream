fea341ae6c54f773154a8a881ea669c1c979c71b
Greg Hall <greghall@bantling.me>
2020-01-15 06:14:56 -0400

    chg

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
 ///////////////////////////////////////////////////////////////////////////////
-// stream is a very simple port of Java streaming to Go that purely functional
-// in a small code base
+// stream is a very simple port of Java streaming to Go that is purely functional
+// in a small code base. It is only for single thread sequential data.
 package stream
