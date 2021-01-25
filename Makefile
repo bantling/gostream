61e5bc98ba3a923d9687eb93a996cbcd6c208d57
Greg Hall <greghall@bantling.me>
2021-01-25 08:52:10 -0400

    Added license

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+# SPDX-License-Identifier: Apache-2.0
+
 reviveConfigFile = revive.toml
 reviveTestConfigFile = revive_test.toml
 
--- doc.go
+++ doc.go
@@ -1,3 +1,4 @@
 // Package gostream is a very simple port of Java streaming to Go that is purely functional
 // in a small code base. It is only for single threaded sequential data.
+// SPDX-License-Identifier: Apache-2.0
 package gostream
